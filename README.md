f38ec175f4c4642f4bcfbf35096acf7e53e83ae7
sugimoto-m <masahiro-s@digirea.com>
2019-04-03 13:37:50 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# react redux test
+
+test.
+
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+
+const webpack = require('webpack');
+const path    = require('path');
+
+module.exports = (env, argv) => {
+    let devmode;
+    let isDevelopment = argv.mode === 'development';
+    if(isDevelopment === true){
+        devmode = 'inline-source-map';
+        console.log('[OK] debug build');
+    }else{
+        devmode = 'none';
+        console.log('[OK] release build');
+    }
+    return {
+        context: path.resolve(__dirname, 'src'),
+        entry: {
+            script: './script.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'public/js'),
+            publicPath: './',
+            filename: 'script.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    include: path.resolve(__dirname, 'src'),
+                    exclude: /node_modules/,
+                    use: [{
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['env']
+                            ]
+                        }
+                    }]
+                }
+            ]
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            open: true,
+            port: 9090,
+            publicPath: '/js/',
+            watchContentBase: true
+        },
+        cache: true,
+        devtool: devmode
+    };
+};
