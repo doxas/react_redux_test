00d857cff6f086c8666bbafc5414e650197d5c00
doxas <eizanco@gmail.com>
2019-04-03 23:08:56 +0900

    import new component in App

--- src/component/app/App.js
+++ src/component/app/App.js
@@ -1,11 +1,19 @@
 
 import React from 'react';
 import './App.css';
+import Splitter from '../splitter/Splitter.js';
 
 export default class App extends React.Component {
     render(){
         return (
-            <div className='app' onClick={this.props.clicker}>{this.props.fuga}</div>
+            <div className='app' onClick={this.props.clicker}>
+                <Splitter
+                    splitDirection='horizontal'
+                    ratio={0.3}
+                    firstChild={<div>{this.props.fuga}</div>}
+                    secondChild={<div>{this.props.fuga}</div>}
+                />
+            </div>
         );
     }
 }
