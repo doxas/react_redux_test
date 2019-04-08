d5014c22cf53c07c27273f4f076cb89b26c6f298
sugimoto-m <masahiro-s@digirea.com>
2019-04-08 14:46:47 +0900

    update app.test.js

--- __tests__/component/app.test.js
+++ __tests__/component/app.test.js
@@ -7,17 +7,30 @@ import {shallow, mount, render} from 'enzyme';
 import App from '../../src/component/app/App.js';
 
 describe('component/app/App.js', () => {
+    let mock = jest.fn();
     test('Splitter が含まれるかどうか', () => {
         // Splitter は props としてメソッド（関数）を受け取る。
-        // shallow を使ってコンポーネントをレンダリングする場合でも、
-        // PropTypes が isRequired な function については、 mock を
-        // 使って代替してやらないと「関数が必要なのに指定されてませんよ！」
-        // と PropTypes が怒るため、Jest の mock 機能で仮の関数（mock）を
-        // 生成して最終的に Splitter.props.onChangeRatio に渡ることになる
+        // これは省略されることを許可されていないので mock を使って代替してやらないと
+        // 「関数が必要なのに指定されてませんよ！」と PropTypes が怒るため、Jest の mock 機能で
+        // 仮の関数（mock）を生成して最終的に Splitter.props.onChangeRatio に渡ることになる
         // changeRatio に指定するようにしている。
-        let mock = jest.fn();
+        // App.js の render メソッドをよーく観察すればわかるが、他のプロパティは省略できるか、
+        // あるいはそのままなにかしらの値が渡るようになっており、onChangeRatio のところだけが
+        // mock を使わないと undefined が渡ってしまう構造になっている。
         let wrapper = shallow(<App changeRatio={mock} />);
+        // Splitter はわざわざ実態を import しなくても名前で一致確認できる
         expect(wrapper.find('Splitter').length).toBe(1);
+    });
+
+    test('App 自身がクリックされたときの props.clickContainer の呼び出し', () => {
+        // App 側では、メソッドが「呼び出されているかどうか」だけをテストする。
+        // 呼び出されている場合にどのような挙動になるべきなのかは、実際には App の
+        // 中身ではなく appReducer.js 側にあるため、そちらでテストするのが正しい。
+        let clickContainerMock = jest.fn();
+        let wrapper = shallow(<App changeRatio={mock} clickContainer={clickContainerMock} />);
+        let wrap = wrapper.find('.app');
+        wrap.simulate('click');
+        expect(clickContainerMock).toHaveBeenCalled();
     });
 });
 
