20b8d9bedd553c9ca7f685859992ffc9c2de0c26
doxas <eizanco@gmail.com>
2019-04-10 02:06:22 +0900

    fix comment

--- __tests__/saga/appSaga.test.js
+++ __tests__/saga/appSaga.test.js
@@ -1,31 +1,64 @@
 
-import {call, put} from 'redux-saga/effects';
+// from package
+import {call, put, takeEvery} from 'redux-saga/effects';
 import '@babel/polyfill';
 
-// target saga of test
+// from source
 import saga, {getRequest} from '../../src/saga/appSaga.js';
 import action from '../../src/action/appAction.js';
 import Api from '../../src/api/index.js';
 
+// saga を利用した非同期処理のテストでは、実際に通信結果がどうなるか、ということについては
+// 無視しつつ、仮にうまくいったとしたらどうなるか、うまくいかなかったとしたらどうなるか、
+// ということをユニットテストによって確かめる必要がある。
+// この「結果がどうなったかを仮の結果を使って指定しつつテストする」を実現するために、
+// JavaScript の Generator 関数の仕組みを利用している。
+
 describe('saga/appSaga.js', () => {
-    test('default export object is instance of Array', () => {
+    test('export default されている saga が配列に入っているか', () => {
         expect(saga).toBeInstanceOf(Array);
+        expect(saga.length).toBe(1);
     });
-    test('getRequest at success', () => {
+    test('配列に入っている saga が正しい構造をしているか', () => {
+        expect(saga[0]).toEqual(takeEvery('REQUEST_TO', getRequest));
+    });
+    test('getRequest が成功した場合の挙動が正しいか', () => {
+        // Generator 関数を生成する
         let generator = getRequest();
+
+        // 最初の yield まで実行し、yield の右辺の式の結果を変数 ret に受ける。
+        // Generator.next() の戻り値は {value: any, done: boolean} というオブジェクトになっている。
+        // つまり以下の next の戻り値の value が、appSaga.getRequest 内に書かれている call の戻り値
+        // であり、同じ条件で生成した call の戻り値との toEqual で比較してテストしている。
         let ret = generator.next();
         expect(ret.value).toEqual(call(Api.getRequest, action.doubleClickRequest().payload));
+
+        // また、Generator.next() は、引数に何かを与えるとそれを「直近の yield の戻り値」として
+        // 使えるという機能がある。これにより、上記の call がどのような結果になったのかを
+        // 擬似的に作ったオブジェクトなどで代替させ、続く次の yield までの処理を実行できる。
         let testResponse = {result: 'something response', error: null};
         ret = generator.next(testResponse);
-        expect(ret.value).toEqual(put({type: 'RESPONSE_SUCCESS', payload: {result: testResponse.result}}));
+        expect(ret.value).toEqual(put({
+            type: 'RESPONSE_SUCCESS',
+            payload: {result: testResponse.result}
+        }));
+
+        // Generator 関数が最後まで実行されたことになっているかを確認
+        ret = generator.next();
+        expect(ret.done).toBeTruthy();
     });
-    test('getRequest at error', () => {
+    test('getRequest がエラーになった場合の挙動が正しいか', () => {
         let generator = getRequest();
         let ret = generator.next();
         expect(ret.value).toEqual(call(Api.getRequest, action.doubleClickRequest().payload));
         let testResponse = {result: null, error: 'something error'};
         ret = generator.next(testResponse);
-        expect(ret.value).toEqual(put({type: 'RESPONSE_FAIL', payload: {error: testResponse.error}}));
+        expect(ret.value).toEqual(put({
+            type: 'RESPONSE_FAIL',
+            payload: {error: testResponse.error}
+        }));
+        ret = generator.next();
+        expect(ret.done).toBeTruthy();
     });
 });
 
--- src/saga/appSaga.js
+++ src/saga/appSaga.js
@@ -7,18 +7,23 @@ import '@babel/polyfill';
 import action from '../action/appAction.js';
 import Api from '../api/index.js';
 
-// この getRequest は、function* とアスタリスク付きで関数が定義されており
-// このような関数を JavaScript では Generator 関数と呼ぶ。
+// この getRequest は、よく見るとわかるが `function*` とアスタリスク付きで関数が
+// 定義されており、このような関数を JavaScript では Generator 関数と呼ぶ。
 // Generator 関数は当然引数を取ることができ、通常の関数と同じように呼び出すことが
 // できるが、saga の仕組み的に「特定の action が発行されたとき」だけに限定して
-// Generator 関数は呼び出されることになる（ここで言う `REQUEST_TO`）ので、
-// 実質引数に action を取っても意味がなく、要するに以下のようにすることは無意味
-// export function* getRequest(action){
+// Generator 関数が呼び出されることになる（ここでは `REQUEST_TO` が対象）ので、
+// 実質引数に action を取っても意味がなく、要するに以下のようにすることは無意味。
+// export function* getRequest(action){ // ← 引数で action を受け取ろうとしている
 //
 // つまり、action を引数から取るのではなく、このファイル内で payload の内容までは
-// 固定で動作させるべきである。
-// export function* getRequest(){
-//     let response = yield call(Api.getRequest, 何らかの固定の定数);
+// 固定で動作させるべきであって、そのためにファイル冒頭で action も import している。
+//
+// 要するにユニットテスト的な観点では、ひとつの Generator 関数を複数のケースで呼び出し、
+// 引数に応じて挙動を分けるとかそういう実装はするべきではなく、対象の関数は常に共通した
+// 挙動をするように設計することが好ましい。
+// その文脈で言うと、以下の getRequest 関数は action.doubleClickRequest で発行される
+// アクション、つまり `REQUEST_TO` 専用とみなすことができるし、他のアクションに対して
+// この関数が反応するようにすべきではない。そうすることで、影響範囲をより狭く限定できる。
 export function* getRequest(){
     // 対象の action の payload を取り出す
     let payload = action.doubleClickRequest().payload;
@@ -27,14 +32,16 @@ export function* getRequest(){
     // 非同期で得られた Api.getRequest の結果を元に処理を分岐する
     if(response != null && response.error == null){
         // error じゃなければこっちの action を発行する
-        yield put({type: 'RESPONSE_SUCCESS', payload: {
-            result: response.result
-        }});
+        yield put({
+            type: 'RESPONSE_SUCCESS',
+            payload: {result: response.result}
+        });
     }else if(response != null && response.error != null){
         // error ならこっちの action を発行する
-        yield put({type: 'RESPONSE_FAIL', payload: {
-            error: response.error
-        }});
+        yield put({
+            type: 'RESPONSE_FAIL',
+            payload: {error: response.error}
+        });
     }
 }
 
