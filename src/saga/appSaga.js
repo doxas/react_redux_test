c8f2598efa729fc201640d20d861f62298f2bb38
doxas <eizanco@gmail.com>
2019-04-10 01:23:44 +0900

    update saga

--- src/saga/appSaga.js
+++ src/saga/appSaga.js
@@ -1,20 +1,39 @@
 
+// from package
 import {call, put, takeEvery} from 'redux-saga/effects';
-import Api from '../api/index.js';
 import '@babel/polyfill';
 
-export function* getRequest(action){
-    // Api.getRequest の結果を受けて……
-    let {result, error} = yield call(Api.getRequest, action.payload);
-    if(error == null){
+// from source
+import action from '../action/appAction.js';
+import Api from '../api/index.js';
+
+// この getRequest は、function* とアスタリスク付きで関数が定義されており
+// このような関数を JavaScript では Generator 関数と呼ぶ。
+// Generator 関数は当然引数を取ることができ、通常の関数と同じように呼び出すことが
+// できるが、saga の仕組み的に「特定の action が発行されたとき」だけに限定して
+// Generator 関数は呼び出されることになる（ここで言う `REQUEST_TO`）ので、
+// 実質引数に action を取っても意味がなく、要するに以下のようにすることは無意味
+// export function* getRequest(action){
+//
+// つまり、action を引数から取るのではなく、このファイル内で payload の内容までは
+// 固定で動作させるべきである。
+// export function* getRequest(){
+//     let response = yield call(Api.getRequest, 何らかの固定の定数);
+export function* getRequest(){
+    // 対象の action の payload を取り出す
+    let payload = action.doubleClickRequest().payload;
+    // 取り出した payload を使って Api.getRequest を実行（非同期）した結果を受けて……
+    let response = yield call(Api.getRequest, payload);
+    // 非同期で得られた Api.getRequest の結果を元に処理を分岐する
+    if(response != null && response.error == null){
         // error じゃなければこっちの action を発行する
         yield put({type: 'RESPONSE_SUCCESS', payload: {
-            result: result
+            result: response.result
         }});
-    }else{
+    }else if(response != null && response.error != null){
         // error ならこっちの action を発行する
         yield put({type: 'RESPONSE_FAIL', payload: {
-            err: error
+            error: response.error
         }});
     }
 }
